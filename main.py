1b8aff595abb55d73cf33a98d14f352952835e43
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-22 00:00:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 
-async def ban_cancel(ctx):
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(intents=intents)
+
+@bot.tree.command(name='role_unban', description='Снятие джоббана')
+async def role_unban(intrecation: discord.Interaction, roleban_id: int):
+    await intrecation.response.send_message(f'Джоббан {roleban_id} снят.')
     
