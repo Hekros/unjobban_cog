c3596e4271458d490ffabd1b28a7ff2b63effde4
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-23 00:58:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy import create_engine, Column, Integer, Text, DateTime
 from sqlalchemy.orm import Session, DeclarativeBase
 
 engine = create_engine(r"sqlite:///C:\Users\sam92\Downloads\SS14.Server_win-x64\data\preferences.db", echo=True)
@@ -14,17 +14,26 @@ bot = commands.Bot(intents=intents)
 class Base(DeclarativeBase):
     pass
 
-class Unban(Base):
+class Ban(Base):
     __tablename__ = 'server_role_ban'
     server_role_ban_id = Column(Integer, primary_key=True, nullable=False)
 
+class Unban(Base):
+    __tablename__ = 'server_role_unban'
+    role_unban_id = Column(Integer, primary_key=True, nullable=False)
+    ban_id = Column(Integer, nullable=False)
+    unban_time = Column(Text, nullable=False)
+    unbanning_admin = Column(Text)
+
 @bot.tree.command(name='role_unban', description='Снятие джоббана')
-async def role_unban(intrecation: discord.Interaction, roleban_id: int):
+async def role_unban(interaction: discord.Interaction, roleban_id: int):
+    username = interaction.user.display_name
     with Session(engine) as session:
-        row = session.query(Unban).filter(Unban.server_role_ban_id == roleban_id).first()
+        row = session.query(Ban).filter(Ban.server_role_ban_id == roleban_id).first()
         if row:
-            session.delete(row)
+            new_row = Unban(role_unban_id=roleban_id, ban_id=roleban_id, unban_time=datetime.now().isoformat(), unbanning_admin=username)
+            session.add(new_row)
             session.commit()
-            await intrecation.response.send_message(f'Джоббан {roleban_id} снят.')
+            await interaction.response.send_message(f'Джоббан {roleban_id} снят.')
         else: 
-            await intrecation.response.send_message('Такого джоббана нет.')
+            await interaction.response.send_message('Такого джоббана нет.')
