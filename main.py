bb9dfb8bb7a474da5d7aef057243efacaf84874e
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-22 19:06:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.orm import Session, DeclarativeBase
+
+engine = create_engine(r"sqlite:///C:\Users\sam92\Downloads\SS14.Server_win-x64\data\preferences.db", echo=True)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(intents=intents)
 
+class Base(DeclarativeBase):
+    pass
+
+class Unban(Base):
+    __tablename__ = 'server_role_ban'
+    server_role_ban_id = Column(Integer, primary_key=True, nullable=False)
+
 @bot.tree.command(name='role_unban', description='Снятие джоббана')
 async def role_unban(intrecation: discord.Interaction, roleban_id: int):
-    await intrecation.response.send_message(f'Джоббан {roleban_id} снят.')
+    with Session(engine) as session:
+        row = session.query(Unban).filter(Unban.server_role_ban_id == roleban_id).first()
+        if row:
+            session.delete(row)
+            session.commit()
+            await intrecation.response.send_message(f'Джоббан {roleban_id} снят.')
+        else: 
+            await intrecation.response.send_message('Такого джоббана нет.')
     
