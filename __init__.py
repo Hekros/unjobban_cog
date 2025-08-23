45e2e6112fad734608576911402d81d51a0c64a2
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-23 19:57:00 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
 from .role_unban_cog import RoleUnBanCog
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 def setup(bot):
     bot.add_cog(RoleUnBanCog(bot))
+    bot.run(TOKEN)
