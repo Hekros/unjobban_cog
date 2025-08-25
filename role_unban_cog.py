e268e67c97c01c112b8a0528d4d88ad17472f130
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-25 17:06:26 +0300

    разделение функций и БД

--- role_unban_cog.py
+++ role_unban_cog.py
@@ -1,28 +1,7 @@
-import os
 import discord
 from discord.ext import commands
 from datetime import datetime
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, Text, DateTime
-from sqlalchemy.orm import Session, DeclarativeBase
-
-engine = create_engine(r"sqlite:///C:\Users\sam92\Downloads\SS14.Server_win-x64\data\preferences.db", echo=True)
-# Базовый класс для создания таблиц
-class Base(DeclarativeBase):
-    pass
-
-# Воссоздаю айди джоббанов из таблицы
-class Ban(Base):
-    __tablename__ = 'server_role_ban'
-    server_role_ban_id = Column(Integer, primary_key=True, nullable=False)
-
-# Воссоздаю анбаны из таблицы полностью
-class Unban(Base):
-    __tablename__ = 'server_role_unban'
-    role_unban_id = Column(Integer, primary_key=True, nullable=False)
-    ban_id = Column(Integer, nullable=False)
-    unban_time = Column(Text, nullable=False)
-    unbanning_admin = Column(Text)
+from data import *
 
 class RoleUnBanCog(commands.Cog):
     def __init__(self, bot: discord.Bot):
