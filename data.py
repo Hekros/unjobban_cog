628ae2d4173ded6a38efcc65c89f1bd202c447c8
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-26 23:43:49 +0300

    WORKING VERSION

--- data.py
+++ data.py
@@ -4,19 +4,18 @@ from sqlalchemy import INT, Text, DateTime, LargeBinary, Text, create_engine, Co
 from sqlalchemy.orm import DeclarativeBase
 
 engine = create_engine(r"sqlite:///C:\Users\sam92\Downloads\SS14.Server_win-x64\data\preferences.db", echo=True)
-# Базовый класс для создания таблиц
+
 class Base(DeclarativeBase):
     pass
 
-# Воссоздаю айди джоббанов из таблицы
 class Ban(Base):
     __tablename__ = 'server_role_ban'
     server_role_ban_id = Column(Integer, primary_key=True, nullable=False)
     hwid = Column(Text)
     player_user_id = Column(Text)
     reason = Column(Text, nullable=False)
+    role_id = Column(Text, nullable=False)
 
-# Воссоздаю анбаны из таблицы полностью
 class Unban(Base):
     __tablename__ = 'server_role_unban'
     role_unban_id = Column(Integer, primary_key=True, nullable=False)
--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 from sqlalchemy import select
 from data import *
 from sqlalchemy.orm import Session
+from roles import ROLES
 
-with Session(engine) as session:
-    find_user_id = session.execute(select(Player.user_id).where(Player.last_seen_user_name == 'AroJan')).scalar()
-    bans = session.execute(select(Ban).outerjoin(Unban, Ban.server_role_ban_id == Unban.ban_id).where(Ban.player_user_id == find_user_id).filter(Unban.ban_id == None)).scalars()
-    for ban in bans:
-        print(ban)
+# with Session(engine) as session:
+#     find_user_id = session.execute(select(Player.user_id).where(Player.last_seen_user_name == ckey)).scalar()
+#     bans = session.execute(select(Ban).outerjoin(Unban, Ban.server_role_ban_id == Unban.ban_id).where(Ban.player_user_id == find_user_id).filter(Unban.ban_id == None)).scalars()
+#     count = 0
+#     # find_role_id = session.execute(select(Ban.role_id).where(Player.user_id == find_user_id)).all()
+#     for ban in bans:
+#         find_role_id = ban.role_id
+#         if ROLES[find_role_id] == otdel:
+#             session.execute(insert(Unban).values(ban_id = ban.server_role_ban_id, unban_time = datetime.datetime.now().isoformat(), unbanning_admin = username))
+
+def get_ckeys():
+    with Session(engine) as session:
+        results = session.execute(select(Player.last_seen_user_name)).scalars()
+        a = []
+        for result in results:
+            a.append(result)
+        return a
+print(get_ckeys())
