f65703619773652f7e124d70fe6625528f58b47b
AroJan <104008943+Hekros@users.noreply.github.com>
2025-08-28 01:12:36 +0300

    roles

--- role_unban_cog.py
+++ role_unban_cog.py
@@ -23,16 +23,14 @@ class RoleUnBanCog(commands.Cog):
     @commands.slash_command(name='role_unban', description='Снятие джоббана')
     async def role_unban(self, ctx: discord.ApplicationContext, 
                          ckey: Option(str, 'Введите сикей', choices=get_ckeys()), # type: ignore
-                         department: Option(str, 'Выберите отдел', choices=['СБ', 'Командование', 'ИИ', 'Медицинский', 'ЦК', 'Инженерный', 'Сервис', 'Научный', 'Юридический', 'Карго'])): # type: ignore
+                         department: Option(str, 'Выберите отдел', choices=dict.keys(ROLES))): # type: ignore
         username = ctx.author.name
         with Session(engine) as session:
             find_user_id = session.execute(select(Player.user_id).where(Player.last_seen_user_name == ckey)).scalar()
             bans = session.execute(select(Ban).outerjoin(Unban, Ban.server_role_ban_id == Unban.ban_id).where(Ban.player_user_id == find_user_id).filter(Unban.ban_id == None)).scalars()
             count = 0
-            # find_role_id = session.execute(select(Ban.role_id).where(Player.user_id == find_user_id)).all()
             for ban in bans:
                 find_role_id = ban.role_id
-                # if ROLES[find_role_id] == otdel:
                 if ban.role_id in ROLES[department]:
                     session.execute(insert(Unban).values(ban_id = ban.server_role_ban_id, unban_time = datetime.datetime.now().isoformat(), unbanning_admin = username))
                     count += 1
@@ -41,9 +39,3 @@ class RoleUnBanCog(commands.Cog):
                 await ctx.respond(f'Джоббан с игрока {ckey} снят.')
             else:
                 await ctx.respond('Такого джоббана нет.')
-            # else:
-            #     unbans = session.execute(select(Ban).join(Unban, Ban.server_role_ban_id == Unban.ban_id).where(Ban.player_user_id == find_user_id)).scalars()
-            #     if count(unbans) == 0:
-            #         await ctx.respond(f'Такого джоббана нет.')
-            #     else:
-            #         await ctx.respond(f'Этот джоббан уже снят.')      
