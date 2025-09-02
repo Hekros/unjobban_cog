da6f93a46f3a4f387622080ea3807d4e85f3dfda
AroJan <104008943+Hekros@users.noreply.github.com>
2025-09-02 23:28:15 +0300

    оптимизация

--- role_unban_cog.py
+++ role_unban_cog.py
@@ -19,23 +19,26 @@ class RoleUnBanCog(commands.Cog):
                          ckey: Option(str, 'Введите сикей'), # type: ignore
                          department: Option(str, 'Выберите отдел', choices=dict.keys(ROLES))): # type: ignore
         a = await player_api.get_player_info(discord_id=ctx.author.id)
-        username = a.get("userId")
+        admin_user_id = a.get("userId")
         async with async_session() as session:
             find_user_i = await session.execute(select(Player.user_id).where(Player.last_seen_user_name == ckey))
             find_user_id = find_user_i.scalar_one_or_none()
             count = 0
             if not find_user_id:
                 await ctx.respond('❌ Такого сикея нет.')
-            else:
-                banss = await session.execute(select(Ban).outerjoin(Unban, Ban.server_role_ban_id == Unban.ban_id).where(Ban.player_user_id == find_user_id).filter(Unban.ban_id == None))
-                bans = banss.scalars()
-                exists = await session.execute(select(Unban.ban_id).where(Unban.ban_id == Ban.server_role_ban_id))
-                for ban in bans:
-                    if ban.role_id in ROLES[department]:
-                        await session.execute(insert(Unban).values(ban_id = ban.server_role_ban_id, unban_time = datetime.datetime.now().isoformat(), unbanning_admin = username))
-                        count += 1
-                await session.commit()
-                if count != 0:
-                    await ctx.respond(f'✅ Джоббан с отдела {department} снят.')
-                if count == 0:
-                    await ctx.respond('❌ Такого джоббана отдела нет.')
+                return
+            banss = await session.execute(select(Ban).outerjoin(Unban, Ban.server_role_ban_id == Unban.ban_id).where(
+                Ban.player_user_id == find_user_id, Unban.ban_id == None,
+                # Ban.role_id == ROLES[department
+                ))
+            bans = banss.scalars()
+            for ban in bans:
+                if ban.role_id in ROLES[department]:
+                    await session.execute(insert(Unban).values(ban_id = ban.server_role_ban_id, unban_time = datetime.datetime.now().isoformat(), unbanning_admin = admin_user_id))
+                    count += 1
+            await session.commit()
+            if count == 0:
+                await ctx.respond('❌ Такого джоббана отдела нет.')
+                return
+            await ctx.respond(f'✅ Джоббан с отдела {department} снят.')
+                
